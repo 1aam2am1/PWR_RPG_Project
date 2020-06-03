2e689654a6b66e877f1ab73a59db2cee11d4dc6f
Karolina-Lach <47970400+Karolina-Lach@users.noreply.github.com>
2020-06-03 17:46:24 +0200

    modify health
    dodanie modify health do kuli z armaty, strzał i ślimaka

--- Assets/Scripts/SnailCombat.cs
+++ Assets/Scripts/SnailCombat.cs
@@ -8,35 +8,52 @@ public class SnailCombat : MonoBehaviour
     private GameObject _deadSnailPrefab;
     [SerializeField]
     private GameObject _shellPrefab;
+    public float m_Damage = 1f;
+    public float m_KnockBack = 10f;
+    // private Snail snail;
+    private void Start()
+    {
+        
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Snail snail = collision.collider.GetComponent<Snail>();
-        if(snail!=null)
+        if (collision.gameObject.tag == "Player")
         {
-            foreach(ContactPoint2D point in collision.contacts)
+            Rigidbody2D playerRigid = collision.collider.GetComponent<Rigidbody2D>();
+            HealthSystem system = collision.gameObject.GetComponent<HealthSystem>();
+            Snail snail = this.gameObject.GetComponent<Snail>();
+            if (playerRigid != null && system != null)
             {
-                Debug.Log(point.normal);
-                Debug.DrawLine(point.point, point.point + point.normal, Color.red, 10);
-                if(point.normal.y >= 0.9f)
+                foreach (ContactPoint2D point in collision.contacts)
                 {
-                    float jumpVelocity = 10;
-                    Rigidbody2D myBody = this.GetComponent<Rigidbody2D>();
-                    Vector2 velocity = myBody.velocity;
-                    velocity.y = jumpVelocity;
-                    myBody.velocity = velocity;
-                    snail.Hurt();
-                    GameObject deadSnail = Instantiate(_deadSnailPrefab, transform.position, Quaternion.identity);
-                    GameObject shell = Instantiate(_shellPrefab, transform.position, Quaternion.identity);
+                    //Debug.Log(point.normal);
+                    Debug.DrawLine(point.point, point.point + point.normal, Color.red, 10);
+                    Debug.Log(point.normal.y);
+                    if (point.normal.y <= -0.9f)
+                    {
+                        float jumpVelocity = 15;
+
+                        playerRigid.AddForce(jumpVelocity * Vector2.up, ForceMode2D.Impulse);
+                        snail.Hurt();
+                        GameObject deadSnail = Instantiate(_deadSnailPrefab, transform.position, Quaternion.identity);
+                        GameObject shell = Instantiate(_shellPrefab, transform.position, Quaternion.identity);
+                    }
+                    else
+                    {
+                        /*float jumpVelocity = 10;
+                        Vector2 velocity = playerRigid.velocity;
+                        velocity.x = point.normal.x * jumpVelocity;
+                        velocity.y = point.normal.y * jumpVelocity;
+                        playerRigid.velocity = velocity;*/
+                        var v = (Vector2)(collision.transform.position - transform.position);
+                        playerRigid.AddForce(m_KnockBack * v.normalized, ForceMode2D.Impulse);
+                        system.TakeDamage(m_Damage);
+                    }
                 }
-                else
-                {
-                    float jumpVelocity = 10;
-                    Rigidbody2D myBody = this.GetComponent<Rigidbody2D>();
-                    Vector2 velocity = myBody.velocity;
-                    velocity.x = point.normal.x * jumpVelocity;
-                    velocity.y = point.normal.y * jumpVelocity;
-                    myBody.velocity = velocity;
-                }
+            }
+            else
+            {
+                Debug.LogError("Player Rigidbody or HealthSystem is null");
             }
         }
     }
