8a57409686b63c2482f4c1202727a4c57c3f7119
Michal_Marszalek <1aam2am1@gmail.com>
2020-06-04 08:58:50 +0200

    Added auto player camera connector.

---
+++ Assets/Scripts/CameraEnemyPlayerConector.cs
@@ -0,0 +1,41 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CameraEnemyPlayerConector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Connect());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Connect()
+    {
+        GameObject player = null;
+        yield return null;
+        while (true)
+        {
+            var objects = GameObject.FindGameObjectsWithTag("Player");
+            if (objects.Count() != 0)
+            {
+                player = objects.First();
+                break;
+            }
+
+            yield return null;
+        }
+
+        FindObjectsOfType<EagleMovement>().ToList().ForEach(n => n.target = player.transform);
+
+        FindObjectsOfType<CinemachineVirtualCamera>().ToList().ForEach(n => n.Follow = player.transform);
+    }
+}
